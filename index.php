bf933740047d326673daca22543f961584cff3f8
joeywangkds <spiderkds@gmail.com>
2023-12-02 21:03:46 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Main template file
+
+@mytheme
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme</title>
+</head>
+<body>
+    
+</body>
+</html>
